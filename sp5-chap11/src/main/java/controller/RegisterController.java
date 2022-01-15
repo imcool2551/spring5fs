a645361ab0c38b5b57cce931e8b7882cf8c04dab
SangWook <imcool2551@gmail.com>
2022-01-15 14:06:14 +0900

    chap12: validation

--- sp5-chap11/src/main/java/controller/RegisterController.java
+++ sp5-chap11/src/main/java/controller/RegisterController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import spring.DuplicateMemberException;
 import spring.MemberRegisterService;
 import spring.RegisterRequest;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegisterController {
@@ -41,9 +44,13 @@ public class RegisterController {
     }
 
     @PostMapping("/register/step3")
-    public String handleStep3(RegisterRequest regReq) {
+    public String handleStep3(@Valid RegisterRequest regReq, Errors errors) {
+        if (errors.hasErrors()) {
+            return "register/step2";
+        }
         try {
             memberRegisterService.regist(regReq);
+            errors.rejectValue("email", "duplicate");
             return "register/step3";
         } catch (DuplicateMemberException ex) {
             return "register/step2";
